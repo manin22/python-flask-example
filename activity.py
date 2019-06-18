41f33f9b26d45cf8a0c71a7947b44cc1411b3284
Martin Nino <martin.nino@seznam.cz>
2019-06-18 17:04:38 +0200

    all done

--- activity.py
+++ activity.py
@@ -3,7 +3,6 @@ import click
 from flask_migrate import Migrate
 from app import create_app, db
 
-@pytest.fixture
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
