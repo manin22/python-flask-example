7a46b0d3dba0e9854848a44aa6c71819ddc2e49d
Martin Nino <martin.nino@seznam.cz>
2019-06-18 16:47:51 +0200

    activity

---
+++ activity.py
@@ -0,0 +1,16 @@
+import os
+import click
+from flask_migrate import Migrate
+from app import create_app, db
+
+@pytest.fixture
+app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db)
+
+@app.route('/')
+def hello():
+    return "Hello World!"
