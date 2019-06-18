f0294fe1cb420b0d6eba4c490e6b7aab42a7296c
Martin Nino <martin.nino@seznam.cz>
2019-06-18 16:46:57 +0200

    app/__init

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite://'
+        'sqlite://data-dev.sqlite'
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
